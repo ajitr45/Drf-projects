17ae178c0e189c9f952d55a83928e6bbeefbcdc9
Ajit kumar <ajit62768@gmail.com>
2026-02-21 11:24:00 +0000

    create employee urls

--- api/urls.py
+++ api/urls.py
@@ -5,7 +5,12 @@ from . import views
 
 urlpatterns = [
 
-    path("students/", views.studentView, name="studentView")
+    path("students/", views.studentView, name="studentView"),
+    path("students/<int:pk>/", views.studentDetailView, name="studentDetailView"),
+
+    path("employees/", views.Employees.as_view()),
+    path("employees/<int:pk>/", views.EmployeeDetail.as_view()),
+
 
 
 
