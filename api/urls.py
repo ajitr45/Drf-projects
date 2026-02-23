86924aefc36827c513e89c4b3dc8bf72825026fd
Ajit kumar <ajit62768@gmail.com>
2026-02-23 06:42:41 +0000

    using Router

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from rest_framework.routers import DefaultRouter
+
+
+router = DefaultRouter()
+router.register('employees', views.EmployeeViewset, basename='employee')
 
 
 
@@ -8,11 +13,11 @@ urlpatterns = [
     path("students/", views.studentView, name="studentView"),
     path("students/<int:pk>/", views.studentDetailView, name="studentDetailView"),
 
-    path("employees/", views.Employees.as_view()),
-    path("employees/<int:pk>/", views.EmployeeDetail.as_view()),
+    # path("employees/", views.Employees.as_view()),
+    # path("employees/<int:pk>/", views.EmployeeDetail.as_view()),
 
 
-
+    path('', include(router.urls))
 
 
 ]
